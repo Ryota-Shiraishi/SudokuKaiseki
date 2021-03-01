c86c9f42d7b491c1ae4e3200d2bd7f8b69a4ae9f
Ryota-Shiraishi <54895997+Ryota-Shiraishi@users.noreply.github.com>
2021-03-02 05:14:14 +0900

    Initial commit

---
+++ Assets/mkDropdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mkDropdown : MonoBehaviour
+{
+    public RectTransform contentRectTransform;
+    public Dropdown dropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <= 8; i++)
+        {
+            Instantiate(dropdown, contentRectTransform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
